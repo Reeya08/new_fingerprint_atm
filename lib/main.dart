2b8d3654bf3db507e4693e8f78222cd74b348e8e
Reeya08 <reeyanadeem3@gmail.com>
2023-11-04 15:23:18 +0500

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:new_fingerprint_atm/presentation/views/splash/splash_view.dart';
+
 import 'firebase_options.dart';
 Future<void> main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
